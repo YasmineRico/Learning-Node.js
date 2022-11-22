800d38392610e19dcfa1e3699223ae09a16427f2
YasmineRico <ricolyasmine@gmail.com>
2022-11-22 10:44:47 -0600

    Creating variables and console logging results

--- global.js
+++ global.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+let hello = "Hello world from Node.js";
+console.log(hello);
+
+// This will go to the hello variable and return "Node.js" from the string
+let justNode = hello.slice(17);
+console.log(justNode);
+
+console.log(`Who let the ${justNode} out?!`)
+
+// console.log("Hello World!");
