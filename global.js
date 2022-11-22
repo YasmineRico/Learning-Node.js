40672eb9e49e883b09c02870230ee770a59c7a33
YasmineRico <ricolyasmine@gmail.com>
2022-11-22 11:28:24 -0600

    using the require function to import the path module

--- global.js
+++ global.js
@@ -8,4 +8,23 @@ console.log(justNode);
 // Using a template literal to show the variable 'justNode' in the string of our console.log
 console.log(`Who let the ${justNode} out?!`)
 
-// console.log("Hello World!");
+console.log("Hello World!");
+
+// *-------------------Using Require Function -----------------------*
+
+// importing the path module from node to use the functions. 
+const path = require("path");
+
+// Will return the specific file global.js rather than the entire path
+console.log(`The file name is ${path.basename(__filename)}`);
+
+// for loop: take a look at the global object and look at all the keys that are in the object. 
+for(let key in global){
+    console.log(key);
+}
+
+// Gives us the directory
+console.log(__dirname);
+
+//Will console the entire path 
+console.log(__filename);
