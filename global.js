12d9f34711b72683aa485ab1c3e14dd6bc8c1715
YasmineRico <ricolyasmine@gmail.com>
2022-11-22 12:53:03 -0600

    console logging process.argv

--- global.js
+++ global.js
@@ -1,30 +1,34 @@
-let hello = "Hello world from Node.js";
-console.log(hello);
+// let hello = "Hello world from Node.js";
+// console.log(hello);
 
-// This will go to the hello variable and return "Node.js" from the string
-let justNode = hello.slice(17);
-console.log(justNode);
+// // This will go to the hello variable and return "Node.js" from the string
+// let justNode = hello.slice(17);
+// console.log(justNode);
 
-// Using a template literal to show the variable 'justNode' in the string of our console.log
-console.log(`Who let the ${justNode} out?!`)
+// // Using a template literal to show the variable 'justNode' in the string of our console.log
+// console.log(`Who let the ${justNode} out?!`)
 
-console.log("Hello World!");
+// console.log("Hello World!");
 
-// *-------------------Using Require Function -----------------------*
+// // *-------------------Using Require Function -----------------------*
 
-// importing the path module from node to use the functions. 
-const path = require("path");
+// // importing the path module from node to use the functions. 
+// const path = require("path");
 
-// Will return the specific file global.js rather than the entire path
-console.log(`The file name is ${path.basename(__filename)}`);
+// // Will return the specific file global.js rather than the entire path
+// console.log(`The file name is ${path.basename(__filename)}`);
 
-// for loop: take a look at the global object and look at all the keys that are in the object. 
-for(let key in global){
-    console.log(key);
-}
+// // for loop: take a look at the global object and look at all the keys that are in the object. 
+// for(let key in global){
+//     console.log(key);
+// }
 
-// Gives us the directory
-console.log(__dirname);
+// // Gives us the directory
+// console.log(__dirname);
 
-//Will console the entire path 
-console.log(__filename);
+// //Will console the entire path 
+// console.log(__filename);
+
+// *-------------------Handling Argument Variables with process.argv-----------------------*
+
+console.log(process.argv);
