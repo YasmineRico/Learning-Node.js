77e2857438b49544c3c4e5e7ccd6a11e66f38120
YasmineRico <ricolyasmine@gmail.com>
2022-11-23 10:03:18 -0600

    create function to output user input with a string

--- global.js
+++ global.js
@@ -1,49 +1,49 @@
-let hello = "Hello world from Node.js";
-console.log(hello);
+// let hello = "Hello world from Node.js";
+// console.log(hello);
 
-// This will go to the hello variable and return "Node.js" from the string
-let justNode = hello.slice(17);
-console.log(justNode);
+// // This will go to the hello variable and return "Node.js" from the string
+// let justNode = hello.slice(17);
+// console.log(justNode);
 
-// Using a template literal to show the variable 'justNode' in the string of our console.log
-console.log(`Who let the ${justNode} out?!`)
+// // Using a template literal to show the variable 'justNode' in the string of our console.log
+// console.log(`Who let the ${justNode} out?!`)
 
-console.log("Hello World!");
+// console.log("Hello World!");
 
-// *-------------------Using Require Function -----------------------*
+// // *-------------------Using Require Function -----------------------*
 
-// importing the path module from node to use the functions. 
-const path = require("path");
+// // importing the path module from node to use the functions. 
+// const path = require("path");
 
-// Will return the specific file global.js rather than the entire path
-console.log(`The file name is ${path.basename(__filename)}`);
+// // Will return the specific file global.js rather than the entire path
+// console.log(`The file name is ${path.basename(__filename)}`);
 
-// for loop: take a look at the global object and look at all the keys that are in the object. 
-for(let key in global){
-    console.log(key);
-}
+// // for loop: take a look at the global object and look at all the keys that are in the object. 
+// for(let key in global){
+//     console.log(key);
+// }
 
-// Gives us the directory
-console.log(__dirname);
+// // Gives us the directory
+// console.log(__dirname);
 
-//Will console the entire path 
-console.log(__filename);
+// //Will console the entire path 
+// console.log(__filename);
 
-// *-------------------Handling Argument Variables with process.argv-----------------------*
+// // *-------------------Handling Argument Variables with process.argv-----------------------*
 
-console.log(process.argv);
+// console.log(process.argv);
 
-// process the values of the flags 
-function grab(flag){
-    let indexAfterFlag = process.argv.indexOf(flag) +1; //Allows to show position values after the flag
-    return process.argv[indexAfterFlag];
-}
+// // process the values of the flags 
+// function grab(flag){
+//     let indexAfterFlag = process.argv.indexOf(flag) +1; //Allows to show position values after the flag
+//     return process.argv[indexAfterFlag];
+// }
 
-let greeting = grab("--greeting"); 
-let user = grab("--user");
+// let greeting = grab("--greeting"); 
+// let user = grab("--user");
 
-console.log(greeting)
-console.log(user);
+// console.log(greeting)
+// console.log(user);
 
 // *-------------------Working with Standard Output-----------------------*
 
@@ -75,6 +75,12 @@ process.stdin.on("data", function(data){
         process.exit();
     }
 });
+
+//will exit the questions and then display the answers along with the string below
+process.on("exit", function(){
+    process.stdout.write("\n\n\n");
+    process.stdout.write(`Go ${answers[1]} ${answers[0]} you can finish writing ${answers[2]}`);
+})
 
 //invoke the questions
 ask();
