e9b2bccd44738944930e4699dade68fc9439b33b
YasmineRico <ricolyasmine@gmail.com>
2022-11-28 15:58:10 -0600

    create setTimeout delay

--- global.js
+++ global.js
@@ -47,44 +47,55 @@
 
 // *-------------------Working with Standard Output-----------------------*
 
-// sending strings to terminal
-process.stdout.write("Hello \n \n");
+// // sending strings to terminal
+// process.stdout.write("Hello \n \n");
 
-//Creating an array of questions
-const questions = [
-    "What is your name?", 
-    "What would you rather be doing?",
-    "What is your preferred programming language?"
-];
+// //Creating an array of questions
+// const questions = [
+//     "What is your name?", 
+//     "What would you rather be doing?",
+//     "What is your preferred programming language?"
+// ];
 
-//Answers to the questions will be put in this empty array
-const answers = [];
+// //Answers to the questions will be put in this empty array
+// const answers = [];
 
-//setting an empty array for a default arg
-function ask(i = 0){
-    process.stdout.write(`\n\n\n ${questions[i]}`);
-    process.stdout.write(`> `);
-}
+// //setting an empty array for a default arg
+// function ask(i = 0){
+//     process.stdout.write(`\n\n\n ${questions[i]}`);
+//     process.stdout.write(`> `);
+// }
 
-//invoke the questions
-ask();
+// //invoke the questions
+// ask();
 
-// Listen for data event on the object using node asynchronously This allows to save the answers.
-process.stdin.on("data", function(data){
-    answers.push(data.toString().trim()); //convert data to string then trim off white space
-    if(answers.length < questions.length){
-        ask(answers.length);
-    }else{
-        process.exit();
-    }
-});
+// // Listen for data event on the object using node asynchronously This allows to save the answers.
+// process.stdin.on("data", function(data){
+//     answers.push(data.toString().trim()); //convert data to string then trim off white space
+//     if(answers.length < questions.length){
+//         ask(answers.length);
+//     }else{
+//         process.exit();
+//     }
+// });
 
-//will exit the questions and then display the answers along with the string below
-process.on("exit", function(){
-    process.stdout.write("\n\n\n");
-    process.stdout.write(`Go ${answers[1]} ${answers[0]}, you can finish writing ${answers[2]} later`);
-})
+// //will exit the questions and then display the answers along with the string below
+// process.on("exit", function(){
+//     process.stdout.write("\n\n\n");
+//     process.stdout.write(`Go ${answers[1]} ${answers[0]}, you can finish writing ${answers[2]} later`);
+// })
 
 
+// *-------------------Creating Delay with setTimeout-----------------------*
 
+
+const waitTime = 3000;
+// Sets the time for 3 seconds 
+console.log(`Setting a ${waitTime / 1000} a second delay`);
+
+//When 3 seconds have passed it will show "done"
+const timerFinished = () => console.log("done");
+
+//first function called for when the timeout has elapsed, then waitTime to define how long it should take.
+setTimeout(timerFinished, waitTime);
 
