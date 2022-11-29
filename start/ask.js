a9abfd831185fb2655f023c8439fbb44d8042e80
YasmineRico <ricolyasmine@gmail.com>
2022-11-29 14:20:21 -0600

    create notes for readlne and interface with rl.question method

--- start/ask.js
+++ start/ask.js
@@ -1,10 +1,14 @@
 // *-------------Collecting Info with Readline--------------*
 
+//readline is a wrapper for stdin/stdout 
 const readline = require("readline");
+
+//create interface for stdin/stdout wrapper readline
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+    input: process.stdin, //maps to process standard input
+    output: process.stdout //maps to process standard output
 });
+
 
 rl.question("How do you like Node? ", (answer) => {
     console.log(`You answered: ${answer}`);
